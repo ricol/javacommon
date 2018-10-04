4fe68adf110ef73b2d953874f0556065fcb80cdd
Ricol Wang <ricol@opensimsim.com>
2018-10-04 13:10:32 +1000

    update.

--- src/au/com/rmit/math/linearalgebra/Matrix.java
+++ src/au/com/rmit/math/linearalgebra/Matrix.java
@@ -26,14 +26,21 @@ public class Matrix
     {
         this.rows = rows;
         this.columns = columns;
-        data = buildMatrix(rows, columns, random);
+        data = buildMatrix(rows, columns, random, true);
+    }
+
+    public Matrix(int rows, int columns, boolean random, boolean nonzero)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        data = buildMatrix(rows, columns, random, nonzero);
     }
 
     public Matrix(int rows, int columns)
     {
         this.rows = rows;
         this.columns = columns;
-        data = buildMatrix(rows, columns, false);
+        data = buildMatrix(rows, columns, false, true);
     }
 
     public double getValue(int row, int column)
@@ -46,16 +53,18 @@ public class Matrix
         data[row][column] = value;
     }
 
-    private double[][] buildMatrix(int rows, int columns, boolean random)
+    private double[][] buildMatrix(int rows, int columns, boolean random, boolean nonzero)
     {
         double[][] m = new double[rows][columns];
+
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < columns; c++)
             {
-                m[r][c] = random ? theRandom.nextDouble() : 0;
+                m[r][c] = (random ? (nonzero ? theRandom.nextDouble() : (theRandom.nextDouble() > 0.7 ? 0 : theRandom.nextDouble())) : 0);
             }
         }
+
         return m;
     }
 
@@ -67,6 +76,7 @@ public class Matrix
     public void show()
     {
         System.out.printf("----------------%d * %d-------------\n", rows, columns);
+
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < columns; c++)
@@ -79,12 +89,10 @@ public class Matrix
 
     public double calculate()
     {
-        if (rows != columns)
-        {
-            return 0;
-        }
+        if (rows != columns) return 0;
 
         double result = 0;
+
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < columns; c++)
@@ -92,6 +100,7 @@ public class Matrix
                 result += Math.pow(data[r][c], Math.pow(-1, r + c));
             }
         }
+
         return result;
     }
 
@@ -117,21 +126,19 @@ public class Matrix
 
     public Matrix plus(Matrix m)
     {
-        if (this.isSameSize(m))
+        if (!this.isSameSize(m)) return null;
+
+        Matrix n = new Matrix(rows, columns);
+
+        for (int r = 0; r < rows; r++)
         {
-            Matrix n = new Matrix(rows, columns);
-            for (int r = 0; r < rows; r++)
+            for (int c = 0; c < columns; c++)
             {
-                for (int c = 0; c < columns; c++)
-                {
-                    n.update(r, c, data[r][c] + m.getValue(r, c));
-                }
+                n.update(r, c, data[r][c] + m.getValue(r, c));
             }
-            return n;
-        } else
-        {
-            return null;
         }
+
+        return n;
     }
 
     public Matrix minus(Matrix m)
@@ -142,6 +149,7 @@ public class Matrix
     public Matrix getNegativeMatrix()
     {
         Matrix n = new Matrix(rows, columns);
+
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < columns; c++)
@@ -149,16 +157,16 @@ public class Matrix
                 n.update(r, c, -data[r][c]);
             }
         }
+
         return n;
     }
 
     public Matrix multiply(Matrix m)
     {
-        if (columns != m.rows)
-        {
-            return null;
-        }
+        if (columns != m.rows) return null;
+
         Matrix n = new Matrix(rows, m.columns);
+
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < m.columns; c++)
@@ -171,6 +179,7 @@ public class Matrix
                 n.update(r, c, value);
             }
         }
+
         return n;
     }
 
@@ -178,21 +187,18 @@ public class Matrix
     {
         int m_rows = rows - 1;
         int m_columns = columns - 1;
-        Matrix m = new Matrix(m_rows, m_columns);
         int m_r = 0;
         int m_c = 0;
+        Matrix m = new Matrix(m_rows, m_columns);
+
         for (int r = 0; r < rows; r++)
         {
-            if (r == row)
-            {
-                continue;
-            }
+            if (r == row) continue;
+
             for (int c = 0; c < columns; c++)
             {
-                if (c == column)
-                {
-                    continue;
-                }
+                if (c == column) continue;
+
                 m.update(m_r, m_c, data[r][c]);
                 m_c += 1;
                 if (m_c >= m_columns)
@@ -202,6 +208,7 @@ public class Matrix
                 }
             }
         }
+
         return m;
     }
 
@@ -211,14 +218,8 @@ public class Matrix
         {
             for (int c = 0; c < columns; c++)
             {
-                if (data[r][c] != 0 && c < r)
-                {
-                    return false;
-                }
-                if (data[r][c] != 1)
-                {
-                    return false;
-                }
+                if (data[r][c] != 0 && c < r) return false;
+                if (data[r][c] != 1) return false;
             }
         }
 
@@ -228,10 +229,7 @@ public class Matrix
     // basic row operation
     public boolean row_operate_swap(int i, int j)
     {
-        if (i >= rows || j >= rows)
-        {
-            return false;
-        }
+        if (i >= rows || j >= rows) return false;
 
         for (int c = 0; c < columns; c++)
         {
@@ -245,10 +243,7 @@ public class Matrix
 
     public boolean row_operate_multiply(int row, double num)
     {
-        if (Math.abs(num) < MathConsts.Minimum || row >= rows)
-        {
-            return false;
-        }
+        if (Math.abs(num) < MathConsts.Minimum || row >= rows) return false;
 
         for (int c = 0; c < columns; c++)
         {
@@ -260,10 +255,7 @@ public class Matrix
 
     public boolean row_operate_add(int from, int to, double num)
     {
-        if (Math.abs(num) < MathConsts.Minimum || from >= rows || to >= rows)
-        {
-            return false;
-        }
+        if (Math.abs(num) < MathConsts.Minimum || from >= rows || to >= rows) return false;
 
         for (int c = 0; c < columns; c++)
         {
@@ -276,6 +268,7 @@ public class Matrix
     public Matrix getCopy()
     {
         Matrix m = new Matrix(rows, columns);
+
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < columns; c++)
@@ -283,6 +276,7 @@ public class Matrix
                 m.update(r, c, data[r][c]);
             }
         }
+
         return m;
     }
 
@@ -302,11 +296,13 @@ public class Matrix
                 double value = data[r][c];
                 if (value == 0) continue;
                 if (value == 1) break;
+
                 for (int column = c; column < columns; column++)
                 {
                     double oldValue = m.getValue(r, column);
                     m.update(r, column, oldValue / value);
                 }
+
                 break;
             }
         }
@@ -315,36 +311,86 @@ public class Matrix
 
         return m;
     }
-    
+
     public Matrix getFullNormalizationMatrix()
     {
+        int r = 0, c = 0;
         Matrix m = this.getCopy();
-        
-        Matrix t = m.getNormalizeMatrix();
-        Matrix sub = t.getSubMatrix(1, 1, rows - 1, columns - 1);
-        
+        System.out.println("getFullNormalizationMatrix...");
+        m.show();
+        m.reorder();
+        System.out.println("reorder:");
+        m.show();
+
+        while (r < rows && c < columns)
+        {
+            Matrix sub = m.getSubMatrix(r, rows - 1, c, columns - 1);
+            Matrix normalize = sub.getNormalizeMatrix();
+            m.updateSubMatrix(r, c, normalize);
+            
+            r += 1;
+            c += 1;
+        }
+
         return m;
     }
-    
+
+    private void reorder()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            int k = i;
+
+            for (int j = i + 1; j < rows; j++)
+            {
+                int compare = this.compareRows(j, k);
+                if (compare == -2)
+                {
+                    System.out.println("Error! row: " + j + " row: " + k);
+                } else if (compare == 1)
+                {
+                    k = j;
+                }
+            }
+
+            if (k != i)
+            {
+                this.row_operate_swap(i, k);
+            }
+        }
+    }
+
+    private int compareRows(int from, int to)
+    {
+        if (from < 0 || to < 0 || from >= rows || to >= rows) return -2;
+
+        for (int c = 0; c < columns; c++)
+        {
+            if (data[from][c] > data[to][c]) return 1;
+            else if (Math.abs(data[from][c] - data[to][c]) < MathConsts.Minimum) continue;
+            else return -1;
+        }
+
+        return 0;
+    }
+
     private int getFirstNonZeroColumnForRow(int row)
     {
         int column = -1;
+
         for (int c = 0; c < columns; c++)
         {
-            if (data[row][c] == 0)
-            {
-                column += 1;
-            } else
-            {
-                break;
-            }
+            if (data[row][c] == 0) column += 1;
+            else break;
         }
+
         return column;
     }
 
     private void all_rows_minus_row(int row)
     {
         int start_from_column = this.getFirstNonZeroColumnForRow(row);
+
         for (int r = 0; r < rows; r++)
         {
             if (r == row) continue;
@@ -355,13 +401,15 @@ public class Matrix
 
     public Matrix getSubMatrix(int from_row, int to_row, int from_column, int to_column)
     {
+        if (to_row >= rows) to_row = rows - 1;
+        if (to_column >= columns) to_column = columns - 1;
         int m_rows = to_row - from_row + 1;
         int m_columns = to_column - from_column + 1;
         if (m_rows < 0 || m_columns < 0) return null;
 
         Matrix m = new Matrix(m_rows, m_columns);
-
         int m_r = 0, m_c = 0;
+
         for (int r = from_row; r <= to_row; r++)
         {
             for (int c = from_column; c <= to_column; c++)
@@ -370,18 +418,19 @@ public class Matrix
                 m_c += 1;
             }
             m_r += 1;
+            m_c = 0;
         }
 
         return m;
     }
-    
+
     public boolean updateSubMatrix(int from_row, int from_column, Matrix m)
     {
-        int to_row = from_row + m.rows;
-        int to_column = from_column + m.columns;
+        int to_row = from_row + m.rows - 1;
+        int to_column = from_column + m.columns - 1;
         if (to_row >= rows || to_column >= columns) return false;
-
         int m_r = 0, m_c = 0;
+
         for (int r = from_row; r <= to_row; r++)
         {
             for (int c = from_column; c <= to_column; c++)
@@ -390,7 +439,9 @@ public class Matrix
                 m_c += 1;
             }
             m_r += 1;
+            m_c = 0;
         }
+
         return true;
     }
 }
@@ -402,8 +453,8 @@ class JavaMain
     {
         System.out.println("Matrix demo...");
 
-        int rows = 5;
-        int columns = 7;
+        int rows = 20;
+        int columns = 23;
         Matrix m = new Matrix(rows, columns, true);
         m.show();
         System.out.println("Calculating..." + m.calculate());
@@ -421,15 +472,7 @@ class JavaMain
         m3.show();
         System.out.println("m1 * m3");
         m1.multiply(m3).show();
-        for (int r = 0; r < m1.rows; r++)
-        {
-            for (int c = 0; c < m1.columns; c++)
-            {
-                System.out.println("Remaining Matrix of " + "row: " + r + " column: " + c);
-                m1.getRemainingMatrix(r, c).show();
-            }
-        }
-
+        m1.getRemainingMatrix(0, 0).show();
         m = new Matrix(rows, columns, true);
         m.show();
         int i = 0, j = rows - 1;
@@ -438,10 +481,11 @@ class JavaMain
         m.show();
 
         System.out.println("Demo normalization...");
-        m = new Matrix(rows, columns, true);
+        m = new Matrix(rows, columns, true, false);
         m.show();
-        m.getSubMatrix(0, 1, 0, 0).show();
-//        System.out.println("Normalizing...");
-//        m.getFullNormalizationMatrix().show();
+        System.out.println("Normalizing...");
+        Matrix normalized = m.getFullNormalizationMatrix();
+        normalized.show();
+        System.out.println("validating..." + (normalized.isSimplified() ? "ok" : "error!"));
     }
 }
